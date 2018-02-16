eca77370df2e55dda05cecd87ca82837da7bea59
Petr Hodina <hodinapetr46@gmail.com>
2018-02-16 21:28:06 +0100

    Fix compilation errors.

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,8 @@ extern crate serde;
 extern crate serde_json;
 
 #[allow(non_snake_case)]
-#[derive(Serialize, Deserialize, Debug)]
-struct MCUConf {
+#[derive(Deserialize, Debug)]
+struct MCUBuilder {
 	Flash : u32,
 	Eeprom : u32,
 	Ram: u32,
@@ -14,26 +14,106 @@ struct MCUConf {
 	IOs : u16,
 	Core : String,
 	Name : String,
-	Line : String,
-	Family : String,
-	Package : String,
+	Line : Option<String>,
+	Family : Option<String>,
+	Package : Option<String>,
 	IPs: Option<Vec<IP>>,
 	Pins: Option<Vec<Pin>>
 }
 
 #[allow(non_snake_case)]
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Deserialize, Debug)]
 struct IP {
 	ConfigFile : String,
 	Name : String
 }
 
 #[allow(non_snake_case)]
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Deserialize, Debug)]
 struct Pin {
 	Name : String,
 	Position : String,
 	Type : String
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct MCUConf {
+	flash : u32,
+	eeprom : u32,
+	ram: u32,
+	frequency : u32,
+	iOs : u16,
+	core : String,
+	name : String,
+	package : Option<String>,
+	periherals: Option<Vec<String>>,
+	middlewares: Option<Vec<String>>,
+	components: Option<Vec<String>>,
+}
+
+enum MCU {
+    STM32, //(MCUConf),
+    STM8, //(MCUConf),
+    ATMEGA,// (MCUConf),
+    MSP430,// (MCUConf)
+}
+
+impl MCU {
+
+	fn create_peripherals (&self) {
+/*
+		for peripheral in self.IPs {
+		match self {
+		    MCU::STM32{} => println!("STM32"),
+		    _ => println!("OTHER"),
+		}	
+		}
+*/		
+	}
+}
+
+impl <'a>MCUBuilder {
+
+	fn new(cfg_file: &str) -> Option<MCU> {
+		// TODO: Return Enum MCU or error
+    	let mcu_builder : MCUBuilder = serde_json::from_str(cfg_file).unwrap();
+    	
+    	let mut mcu = if mcu_builder.Name.starts_with("STM32") {
+
+    		MCU::STM32
+	    	}
+	    else if mcu_builder.Name.starts_with("STM8") {
+
+	    	MCU::STM8
+	    	}
+	    else if mcu_builder.Name.starts_with("ATMEGA") {
+
+	    	MCU::ATMEGA
+	    	}
+	    else if mcu_builder.Name.starts_with("MSP430") {
+
+    		MCU::MSP430
+	    	}
+	    else {
+	    	    
+    		return None;
+	    	};
+
+	    mcu.create_peripherals();
+	    Some(mcu)
+	}
+
+	fn create_peripheral(&mut self, name: &'a str) {
+
+		}
+
+	fn create_middleware(&mut self, name: &'a str) {
+
+		}
+
+	fn create_component(&mut self, name: &'a str) {
+
+		}
 }
 
 
@@ -43,7 +123,7 @@ mod tests {
 	use super::*;
 
     #[test]
-    fn mcuconf_ok() {
+    fn mcubuilder_ok() {
     	let json = r#"{ "Core" : "ARM Cortex-M0",
     					"Eeprom" : 0,
     					"Family" : "STM32F0",
@@ -55,18 +135,18 @@ mod tests {
     					"Package" : "LQFP48",
     					"Ram" : 4 }"#;
     	
-    	let mcu_conf : MCUConf = serde_json::from_str(json).unwrap();
+    	let mcu_builder : MCUBuilder = serde_json::from_str(json).unwrap();
 
-    	assert_eq!(mcu_conf.Core, "ARM Cortex-M0");
-    	assert_eq!(mcu_conf.Eeprom, 0);
-    	assert_eq!(mcu_conf.Family, "STM32F0");
-    	assert_eq!(mcu_conf.Flash, 32);
-    	assert_eq!(mcu_conf.Frequency, 48);
-    	assert_eq!(mcu_conf.IOs, 39);
-    	assert_eq!(mcu_conf.Line, "STM32F0x0 Value Line");
-    	assert_eq!(mcu_conf.Name, "STM32F030C6Tx");
-    	assert_eq!(mcu_conf.Package, "LQFP48");
-    	assert_eq!(mcu_conf.Ram, 4);
+    	assert_eq!(mcu_builder.Core, "ARM Cortex-M0");
+    	assert_eq!(mcu_builder.Eeprom, 0);
+    	assert_eq!(mcu_builder.Family, "STM32F0");
+    	assert_eq!(mcu_builder.Flash, 32);
+    	assert_eq!(mcu_builder.Frequency, 48);
+    	assert_eq!(mcu_builder.IOs, 39);
+    	assert_eq!(mcu_builder.Line, "STM32F0x0 Value Line");
+    	assert_eq!(mcu_builder.Name, "STM32F030C6Tx");
+    	assert_eq!(mcu_builder.Package, "LQFP48");
+    	assert_eq!(mcu_builder.Ram, 4);
     }
 
     #[test]
