6f32cd2b17cc1ffd9e066b1692d48052e1c23d7f
Petr Hodina <hodinapetr46@gmail.com>
2018-02-18 16:52:00 +0100

    Refactor struct MCUBuilder. Add samples for unit tests.

--- src/lib.rs
+++ src/lib.rs
@@ -4,21 +4,31 @@ extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
 
+use std::error::Error;
+use std::fs::File;
+use std::path::Path;
+
+#[allow(non_snake_case)]
+#[derive(Deserialize, Debug)]
+struct MCU {
+    Flash : u32,
+    Eeprom : u32,
+    Ram: u32,
+    Frequency : u32,
+    IOs : u16,
+    Core : String,
+    Name : String,
+    Line : Option<String>,
+    Family : Option<String>,
+    Package : Option<String>
+}
+
 #[allow(non_snake_case)]
 #[derive(Deserialize, Debug)]
 struct MCUBuilder {
-	Flash : u32,
-	Eeprom : u32,
-	Ram: u32,
-	Frequency : u32,
-	IOs : u16,
-	Core : String,
-	Name : String,
-	Line : Option<String>,
-	Family : Option<String>,
-	Package : Option<String>,
-	IPs: Option<Vec<IP>>,
-	Pins: Option<Vec<Pin>>
+    Mcu: MCU,
+    IPs: Option<Vec<IP>>,
+    Pins: Option<Vec<Pin>>
 }
 
 #[allow(non_snake_case)]
@@ -50,7 +60,7 @@ struct MCUConf {
 	middlewares: Option<Vec<String>>,
 	components: Option<Vec<String>>,
 }
-
+/*
 enum MCU {
     STM32, //(MCUConf),
     STM8, //(MCUConf),
@@ -61,19 +71,43 @@ enum MCU {
 impl MCU {
 
 	fn create_peripherals (&self) {
-/*
+
 		for peripheral in self.IPs {
 		match self {
 		    MCU::STM32{} => println!("STM32"),
 		    _ => println!("OTHER"),
 		}	
 		}
-*/		
+		
 	}
 }
-
+*/
 impl <'a>MCUBuilder {
 
+    fn new(path: &Path) -> Result<MCUBuilder, Box<Error>> {
+        let file = File::open(path)?;
+        let mcu_builder = serde_json::from_reader(file)?;
+
+        Ok(mcu_builder)
+    }
+
+    fn finish (self) -> MCUConf {
+
+        MCUConf {
+            flash : self.Mcu.Flash,
+	    eeprom : self.Mcu.Eeprom,
+	    ram: self.Mcu.Ram,
+	    frequency : self.Mcu.Frequency,
+	    iOs : self.Mcu.IOs,
+	    core : self.Mcu.Core,
+	    name : self.Mcu.Name,
+	    package : self.Mcu.Package,
+	    periherals: None,
+	    middlewares: None,
+	    components: None
+        }
+    }
+        /*
 	fn new(cfg_file: &str) -> Option<MCU> {
 		// TODO: Return Enum MCU or error
     	let mcu_builder : MCUBuilder = serde_json::from_str(cfg_file).unwrap();
@@ -101,7 +135,7 @@ impl <'a>MCUBuilder {
 
 	    mcu.create_peripherals();
 	    Some(mcu)
-	}
+	}*/
 
 	fn create_peripheral(&mut self, name: &'a str) {
 
@@ -120,11 +154,21 @@ impl <'a>MCUBuilder {
 #[cfg(test)]
 mod tests {
 
-	use super::*;
+    use super::*;
+    
+    #[test]
+    fn mcubuilder_load() {
 
+        let sample = Path::new("./samples/STM32F030C6Tx.json");
+        let mcubuilder = MCUBuilder::new(sample);
+        
+        assert_eq!(mcubuilder.is_ok(), true);
+    }
+        
     #[test]
     fn mcubuilder_ok() {
-    	let json = r#"{ "Core" : "ARM Cortex-M0",
+
+    	let json = r#"{ "Mcu": {"Core" : "ARM Cortex-M0",
     					"Eeprom" : 0,
     					"Family" : "STM32F0",
     					"Flash" : 32,
@@ -133,20 +177,20 @@ mod tests {
     					"Line" : "STM32F0x0 Value Line",
     					"Name" : "STM32F030C6Tx",
     					"Package" : "LQFP48",
-    					"Ram" : 4 }"#;
+    					"Ram" : 4 }}"#;
     	
     	let mcu_builder : MCUBuilder = serde_json::from_str(json).unwrap();
 
-    	assert_eq!(mcu_builder.Core, "ARM Cortex-M0");
-    	assert_eq!(mcu_builder.Eeprom, 0);
-    	assert_eq!(mcu_builder.Family, "STM32F0");
-    	assert_eq!(mcu_builder.Flash, 32);
-    	assert_eq!(mcu_builder.Frequency, 48);
-    	assert_eq!(mcu_builder.IOs, 39);
-    	assert_eq!(mcu_builder.Line, "STM32F0x0 Value Line");
-    	assert_eq!(mcu_builder.Name, "STM32F030C6Tx");
-    	assert_eq!(mcu_builder.Package, "LQFP48");
-    	assert_eq!(mcu_builder.Ram, 4);
+    	assert_eq!(mcu_builder.Mcu.Core, "ARM Cortex-M0");
+    	assert_eq!(mcu_builder.Mcu.Eeprom, 0);
+    	assert_eq!(mcu_builder.Mcu.Family.unwrap(), "STM32F0");
+    	assert_eq!(mcu_builder.Mcu.Flash, 32);
+    	assert_eq!(mcu_builder.Mcu.Frequency, 48);
+    	assert_eq!(mcu_builder.Mcu.IOs, 39);
+    	assert_eq!(mcu_builder.Mcu.Line.unwrap(), "STM32F0x0 Value Line");
+    	assert_eq!(mcu_builder.Mcu.Name, "STM32F030C6Tx");
+    	assert_eq!(mcu_builder.Mcu.Package.unwrap(), "LQFP48");
+    	assert_eq!(mcu_builder.Mcu.Ram, 4);
     }
 
     #[test]
